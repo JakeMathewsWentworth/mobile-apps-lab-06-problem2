72ff0da82948cc5975f47a1720a1accd8277e4c8
Jake Mathews <mathewsj2@wit.edu>
2018-08-15 14:24:20 -0400

    Upload

---
+++ app/src/main/java/edu/wit/mobileapp/lab6_problem2/ListItem.java
@@ -0,0 +1,13 @@
+package edu.wit.mobileapp.lab6_problem2;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by mathewsj2 on 2/20/18.
+ */
+
+public class ListItem {
+    public Bitmap image;
+    public String name;
+    public String comment;
+}
